80a66e3cc14061240dcdf592202922f0a96134fc
bdcostin <bailey.costin@gmail.com>
2019-04-01 14:23:15 -0400

    Initial Django project setup

---
+++ core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from autoslug import AutoSlugField
+
+# Create your models here.
+
+class User(AbstractUser):
+    '''This model represents the custom user model'''
+    username = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+    slug = AutoSlugField(populate_from=username)
+    email = models.CharField(max_length=50, null=False, blank=False)
+    gender_pronouns = models.CharField(max_length=20, null=True, blank=True)
+    date_created = models.DateField(auto_now_add=True, blank=True)
+    about = models.TextField(max_length=1000, null=True, blank=True)
+
+class Language(models.Model):
+    '''This model represents the language category'''
+    name = models.CharField(max_length=50)
+
+class Snippet(models.Model):
+    '''This model represents the users post'''
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, null=False, blank=False)
+    languages = models.ManyToManyField(Language)
+    post_content = models.TextField(max_length=5000)
+    slug = AutoSlugField(populate_from=title)
+    created_at = models.DateTimeField(auto_now=True)
+    source_id = models.CharField(null=True)
