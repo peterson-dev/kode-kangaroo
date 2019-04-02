935c9d7918772a6595b190160bcb6e2d5d6f2d56
bdcostin <bailey.costin@gmail.com>
2019-04-02 17:32:36 -0400

    fixed view issue

--- codevault/urls.py
+++ codevault/urls.py
@@ -23,8 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
     path('', RedirectView.as_view(url='/core/', permanent=True)),
-    path('accounts/', 
-    include('registration.backends.default.urls')),
+    path('accounts/', include('registration.backends.default.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import view, FolderDetailView, SnippetDetailView
+from . import views 
+from core.views import FolderDetailView, SnippetDetailView
 
 urlpatterns = [
     # path('', views.HomePageView.as_view(model=UserPost), name='index'),
-    path('snippets/<slug:slug>/', SnippetDetailView.as_view(), name='snippet-detail'),
-    path('folders/<int:pk>/', FolderDetailView.as_view(), name='folder-detail'),
+    path('snippets/<slug:slug>/', views.SnippetDetailView.as_view(), name='snippet-detail'),
+    path('folders/<int:pk>/', views.FolderDetailView.as_view(), name='folder-detail'),
 ]
