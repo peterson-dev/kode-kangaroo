fea73365d684487b55f671f62ee97c0610bc5850
dmpeterson317 <dmpeterson317@gmail.com>
2019-04-02 16:50:06 -0400

    add class based views and one url for folder detail

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from . import view, FolderDetailView, SnippetDetailView
 
 urlpatterns = [
     # path('', views.HomePageView.as_view(model=UserPost), name='index'),
-    
+    path('snippets/<slug:slug>/', SnippetDetailView.as_view(), name='snippet-detail'),
+    path('folders/<int:pk>/', FolderDetailView.as_view(), name='folder-detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from core.models import Folder, Snippet
 
-# Create your views here.
+class FolderDetailView(LoginRequiredMixin, DetailView):
+    model = Folder
+    template_name = 'core/folder_contents.html'
+
+class SnippetDetailView(LoginRequiredMixin, DetailView):
+    model = Snippet
 
