00ffc4d65a88448190f607da0708d31b217e0352
dmpeterson317 <dmpeterson317@gmail.com>
2019-04-02 21:29:58 -0400

    quick change to index and base template/urls

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views 
-from core.views import FolderDetailView, SnippetDetailView
 
 urlpatterns = [
-    # path('', views.HomePageView.as_view(model=UserPost), name='index'),
+    path('', views.index, name='index'),
+    path('snippets/', views.SnippetListView.as_view(), name='snippet_list'),
     path('snippets/<slug:slug>/', views.SnippetDetailView.as_view(), name='snippet-detail'),
     path('folders/<int:pk>/', views.FolderDetailView.as_view(), name='folder-detail'),
 ]
