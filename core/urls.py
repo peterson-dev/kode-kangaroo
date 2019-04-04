07946c75636e398375ea83f6136cf38a84044971
bdcostin <bailey.costin@gmail.com>
2019-04-04 12:12:14 -0400

    Update edit, delete, and save function

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('snippets/', views.SnippetListView.as_view(), name='snippet_list'),
     path('snippets/<slug:slug>/', views.SnippetDetailView.as_view(), name='snippet-detail'),
+    path('snippet/<slug:slug>/', views.SnippetUpdateView.as_view(), name='snippet-update'),
     path('folders/<int:pk>/', views.FolderDetailView.as_view(), name='folder-detail'),
+    path('snippet/<slug:slug>/delete', views.SnippetDeleteView.as_view(), name='confirm-snippet-delete'),
 ]
---
+++ static/js/main.js
@@ -0,0 +1,34 @@
+// import { format } from "util";
+// globals fetch, Cookies
+
+function query (selector) {
+  return document.querySelector(selector)
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  for (let form of query('.delete-snippet') {
+    form.addEventListener('submit', function (event){
+        event.preventDefault()
+        const csrftoken = Cookies.get('csrftoken')
+        fetch(form.action, { 
+            method: 'POST' 
+            headers: { 'X-CSRFToken': csrftoken, 'X-Requested-With': 'XMLHttpRequest' }
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw Error(response.statusText)
+          }
+          return response.json()
+        })
+        .then(json => {
+          query('#snippet-${form.dataset['title']}').remove()
+        })
+    })
+  }
+})
+
+// .then(json => {
+//     query('#snippet-${form.dataset['title']}').remove()
+//   })
+
+// Working with snippet_list - delete form/button - is the query arg correct? Also need to add unique identifier to the model.
