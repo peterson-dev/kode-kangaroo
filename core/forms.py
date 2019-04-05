a857ec0120f29c4015be78a4e88072deae35f408
dmpeterson317 <dmpeterson317@gmail.com>
2019-04-05 16:15:03 -0400

    create new snippet form

---
+++ core/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from core.models import Snippet
+
+
+class NewSnippetForm(forms.Form):
+    title = forms.CharField(
+        label='Title',
+        max_length=55,
+        widget=forms.TextInput(attrs={'required': True}),
+    )
+    language = forms.ChoiceField(
+        label='Language',
+        choices=Snippet.LANG_CHOICES, 
+        required=True,
+    )
+    post_content = forms.CharField(
+        label='',
+        max_length=1000,
+        widget=forms.Textarea(attrs={'placeholder': 'add a new snippet','required': True}),
+    )
+
+    def save(self, user, *args, **kwargs):
+        if self.is_valid():
+            data = {
+                'title': self.cleaned_data['title'], 
+                'language': self.cleaned_data['language'], 
+                'post_content': self.cleaned_data['post_content'],
+                }
+            data.update(kwargs)
+            print(data)
+            return Snippet.objects.create(user=user, **data)
+        return None
