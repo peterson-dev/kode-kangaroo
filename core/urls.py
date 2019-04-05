33c80875aaf6820c314da46c9677dec19d2f2324
dmpeterson317 <dmpeterson317@gmail.com>
2019-04-05 16:14:10 -0400

    add create url path

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('snippets/', views.SnippetListView.as_view(), name='snippet_list'),
+    path('snippets/new/', views.new_snippet, name='new-snippet'),
     path('snippets/<slug:slug>/', views.SnippetDetailView.as_view(), name='snippet-detail'),
     path('snippet/<slug:slug>/', views.SnippetUpdateView.as_view(), name='snippet-update'),
     path('folders/<int:pk>/', views.FolderDetailView.as_view(), name='folder-detail'),
