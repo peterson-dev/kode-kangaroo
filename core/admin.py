29795d29bc55696acea04416a2de5dcb1dc9c2aa
dmpeterson317 <dmpeterson317@gmail.com>
2019-04-05 16:13:25 -0400

    remove language admin model

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Snippet, Language, Folder
+from .models import User, Snippet, Folder
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
@@ -10,9 +10,9 @@ class SnippetAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'post_content', 'source_id')
     exclude = ('slug',)
 
-@admin.register(Language)
-class LanguageAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+# @admin.register(Language)
+# class LanguageAdmin(admin.ModelAdmin):
+#     list_display = ('name',)
 
 @admin.register(Folder)
 class FolderAdmin(admin.ModelAdmin):
