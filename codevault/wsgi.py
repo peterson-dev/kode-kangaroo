9421acb56e0c16144f4f2e808e7edfc4896d0d78
Dan Dev <peterzen.dev@gmail.com>
2019-05-16 18:17:42 -0400

    download allauth, change project name, change all files with new name

--- codevault/wsgi.py
+++ codevault/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for codevault project.
+WSGI config for kodekangaroo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codevault.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kodekangaroo.settings')
 
 application = get_wsgi_application()
