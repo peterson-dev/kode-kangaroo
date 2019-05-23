5d4c514f610d6674fb65abd823cf0b03398cdc8d
Dan Dev <peterzen.dev@gmail.com>
2019-05-23 16:19:02 -0400

    add logic to allow new snippet form to post from most views

--- core/views.py
+++ core/views.py
@@ -11,8 +11,6 @@ from core.templatetags import snippet_tags
 from core.filters import SnippetFilter, SnippetListFilter
 
 
-# from django.shortcuts import redirect, render
-
 class SnippetListView(LoginRequiredMixin, ListView):
     model = Snippet
     context_object_name = 'snippets'
@@ -22,44 +20,51 @@ class SnippetListView(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['form'] = NewSnippetForm
         context['filter'] = SnippetListFilter(self.request.GET, queryset=self.get_queryset())
-
         return context
         
 
 @require_http_methods(['POST'])
 @login_required
 def new_snippet(request):
-    # if request.content_type == 'application/json':
-    #     body = json.loads(request.body)
-    # else:
-    #     body = request.POST
-    
     form = NewSnippetForm(request.POST)
-
     if form.is_valid():
         form.save(user=request.user)
-
-        # if request.is_ajax():
-        #     return render(request, 'partials/snippets.html', {'snippet': snippet})
-
     return redirect('snippet_list')
 
-class FolderDetailView(LoginRequiredMixin, DetailView):
-    model = Folder
-    template_name = 'core/folder_contents.html'
 
-    # def get_context_data(self, **kwargs):
-    
+class AllPublicSnippets(LoginRequiredMixin, ListView):
+    model = Snippet
+    context_object_name = 'snippets'
+    template_name = 'core/kommunity_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = NewSnippetForm
+        context['filter'] = SnippetFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
 
 class SnippetDetailView(LoginRequiredMixin, DetailView):
     model = Snippet
     template_name = 'core/snippet_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = NewSnippetForm
+        return context
+
 
 class SnippetUpdateView(LoginRequiredMixin, UpdateView):
     model = Snippet
     template_name = 'core/update_snippet.html'
     fields = ['title', 'post_content', 'public']
     success_url = reverse_lazy('snippet_list')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = NewSnippetForm
+        return context
+
 
 class SnippetDeleteView(LoginRequiredMixin, DeleteView):
     model = Snippet
@@ -67,30 +72,20 @@ class SnippetDeleteView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('snippet_list')
 
     def form_validation(self, request):
-
         if self.request.is_ajax():
             return JsonResponse({"complete": True})
-
         return redirect('core/snippet_list.html')
 
 
-def user_public_snippets(request, pk):
-    template_name = 'core/user_public_detail.html'
-    snippets = Snippet.objects.filter(public=True, user=pk)
+class FolderDetailView(LoginRequiredMixin, DetailView):
+    model = Folder
+    template_name = 'core/folder_contents.html'
 
-    context = {
-        'snippets': snippets,
-    }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = NewSnippetForm
+        return context
 
-    return render(request, 'core/user_public_detail.html', context)
-
-
-def all_public_snippets(request):
-    template_name = 'core/kommunity_list.html'
-    snippets = Snippet.objects.filter(public=True)
-    snippets_filter = SnippetFilter(request.GET, queryset=snippets)
-
-    return render(request, 'core/kommunity_list.html', {'filter': snippets_filter})
 
 class SnippetCopyView(LoginRequiredMixin, CreateView):
     model = Snippet
@@ -98,8 +93,15 @@ class SnippetCopyView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('snippet_list')
 
     def form_validation(self, request):
-
         if self.request.is_ajax():
             return JsonResponse({"complete": True})
-
         return redirect('core/snippet_list.html')
+
+
+def user_public_snippets(request, pk):
+    template_name = 'core/user_public_detail.html'
+    snippets = Snippet.objects.filter(public=True, user=pk)
+    context = {
+        'snippets': snippets,
+    }
+    return render(request, 'core/user_public_detail.html', context)
