0ceee384d37cac0f73b543819af881db2a6aa138
Dan Dev <peterzen.dev@gmail.com>
2019-05-31 22:02:54 -0400

    my first cypress test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,17 @@
-describe('My First Test', function() {
-  it('Visits the Kitchen Sink', function() {
-    cy.visit('https://kode-kangaroo.herokuapp.com')
+/// <reference types="Cypress" />
+
+//my first cypress test
+describe('Log in Admin', function() {
+  it('should log in existing user and travel to index', function() {
+    cy.visit('http://localhost:8000/')
+    //cy.visit('http://kode-kangaroo.herokuapp.com')
+    cy.get('#id_username')
+      .type('admin')
+      .get('#id_password')
+      .type('1234')
+      .get('#cy-login').click()
+
+    //Should be on a new URL which includes '/snippets/'
+    cy.url().should('include', '/snippets/')
   })
 })
